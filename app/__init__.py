adb81764a37652964b26110aef3b1ca1a22152f3
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2024-06-17 00:05:43 -0700

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-from gevent import monkey
-monkey.patch_all()
 import os
 import base64
 from io import BytesIO
--- run.py
+++ run.py
@@ -3,4 +3,6 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app)
+    import eventlet
+    eventlet.monkey_patch()
+    socketio.run(app, host="0.0.0.0", port=5000)
