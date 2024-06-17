ab6758d37f533095392e9d593918f0ecb8b797f1
Jarod Johnson <93343444+jarod-johnson-23@users.noreply.github.com>
2024-06-17 00:24:28 -0700

    aweawefasdva

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
+import eventlet
+eventlet.monkey_patch()
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    import eventlet
-    eventlet.monkey_patch()
     socketio.run(app, host="0.0.0.0", port=5000)
